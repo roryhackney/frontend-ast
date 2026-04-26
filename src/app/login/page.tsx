0a45ec13f80fdcd50b315ec8ba90817dba27c03b
roryhackney <roryhackney@gmail.com>
2026-04-26 11:59:49 -0700

    Set up imports, simple login route, simple button component to test it works

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+"use strict";
+import Button from "@/components/Button";
+
+export default function Login() {
+  return (
+      <main>
+        <h1>Log In</h1>
+        <form>
+            <Button/>
+        </form>
+      </main>
+  );
+}
